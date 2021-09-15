4818cca81d814a46be9194f59237681c1d556353
Priyanshu Nandagawali <72712134+priyanshupn@users.noreply.github.com>
2021-09-15 23:19:48 +0530

    3 files uploaded

---
+++ assign.py
@@ -0,0 +1,7 @@
+print ('This is my first SE assignment')
+print ("Installing git and working on git commands")
+a = 10
+b = 20
+c = 30
+print (a + b)
+print (a + b + c)
---
+++ myfirst.py
@@ -0,0 +1,4 @@
+
+print ("Hello this is myfirst git")
+print ("\n Welcome to my world")
+
